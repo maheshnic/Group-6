604f5e62e657b59af577da08eb58a4f279a1695f
Biswashreya <biswashreyaa@gmail.com>
2025-07-01 13:10:44 +0530

    Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        scroll: {
+          '0%': { transform: 'translateY(0%)' },
+          '100%': { transform: 'translateY(-50%)' },
+        },
+      },
+      animation: {
+        'scroll-slow': 'scroll 15s linear infinite',
+        'scroll-fast': 'scroll 8s linear infinite',
+        'scroll-medium': 'scroll 10s linear infinite',
+      },
+    },
   },
   plugins: [],
-}
+};
